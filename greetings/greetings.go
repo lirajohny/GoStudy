d086a3174a8401f34d03dfd26bc333f6f323de86
lirajohny <johnylira.dev@gmail.com>
2025-02-09 11:39:59 -0300

    running a module v3

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -2,6 +2,7 @@ package greetings
 
 import "fmt"
 
+
 // Hello returns a greeting for the named person.
 func Hello(name string) string {
     // Return a greeting that embeds the name in a message.
---
+++ hello/go.mod
@@ -0,0 +1,3 @@
+module github.com/lirajohny/GoStudy/tree/main/hello
+
+go 1.23.0
